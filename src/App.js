90131070e25dec7d1f98304e941f7672728f1269
Su0414 <susanaik.com>
2018-11-26 11:30:38 -0800

    add layout for pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
+      {/* Uses a header that scrolls with the text, rather than staying locked at the top */}
+      <div className="demo-big-content">
+          <Layout>
+              <Header title="My Portfolio" scroll>
+                  <Navigation>
+                      <a href="#">Contact</a>
+                      <a href="#">Projects</a>
+                      <a href="#">Blog</a>
+                      <a href="#">Resume</a>
+                  </Navigation>
+              </Header>
+              <Drawer title="My Portfolio">
+                  <Navigation>
+                      <a href="#">Contact</a>
+                      <a href="#">Projects</a>
+                      <a href="#">Blog</a>
+                      <a href="#">Resume</a>
+                  </Navigation>
+              </Drawer>
+              <Content>
+                  <div className="page-content" />
+              </Content>
+          </Layout>
+      </div>
+
       </div>
     );
   }
